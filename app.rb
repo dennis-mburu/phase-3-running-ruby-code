da78850fb3ffaa9f1dc54e34d18fd59a783a4cd1
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-08-12 09:43:56 +0300

    Assignment Complete

---
+++ app.rb
@@ -0,0 +1,28 @@
+puts ('Hello world!')
+puts "Hello world!"
+puts "Hello world!"
+
+
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
+
+
+p [1,2,3]
+puts [1,2,3]
+puts [1,2,3].inspect
+# print [1,2,3]
+
+pp [1,2,3]
+
+pp "Hello world!"
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+p [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# print [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# print "Hello World!\n"
+puts "Hello World!"
+print "Pass this test, please."
